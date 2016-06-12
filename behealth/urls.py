2bc63b8d1433a70320832b77bfea586bddbca288
Fabio Godinho <fabio.godinho92@gmail.com>
2016-06-12 12:49:48 +0200

    Renaming ipa4life references to behealth
    Renaming ipa4life references to behealth

--- behealth/urls.py
+++ behealth/urls.py
@@ -1,4 +1,4 @@
-"""ipa4life URL Configuration
+"""behealth URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.9/topics/http/urls/
--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ipa4life.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "behealth.settings")
 
     from django.core.management import execute_from_command_line
 

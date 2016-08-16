b5cbb71d064a800b969b9f4bf49f20eb64e71a7d
Hugues Chips <dd.durrenberger@gmail.com>
2016-08-16 17:35:05 +0200

    New branch
    With all about calcultation

--- calculation/calculMethods.py
+++ calculation/calculMethods.py
@@ -3,11 +3,13 @@ from datalayer import calculConst
 def calculBMI(weight, size):
     size = size / 100
     return weight / (size * size)
-    
+
 def calculEER(age, weight, size, sex, activity):
     consts = calculConst.getConsts(sex, age)
     bmi = calculBMI(weight, size)
     indice = calculConst.getEERActivityIndice(age, bmi, sex, activity)
     sizeInMeter = size / 100
-    
-    return (consts[0] - consts[1] * age) + indice * (consts[2] * weight) + (consts[3] * sizeInMeter)
+    EER = (consts[0] - consts[1] * age)
+    EER += indice * (consts[2] * weight)
+    EER += (consts[3] * sizeInMeter)
+    return EER
